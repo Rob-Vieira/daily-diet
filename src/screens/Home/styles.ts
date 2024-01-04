3fd29a074796673bfb9d99cbdcffb0eae8c8c542
Robson Vieira <72048753+Rob-Vieira@users.noreply.github.com>
2024-01-04 15:07:18 -0300

    Initial commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Text = styled.Text`
+    font-size: 30px;
+`;
+
+export {
+    Container,
+    Text
+};
